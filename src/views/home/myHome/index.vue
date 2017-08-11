467144b731171b918385b36e5ca4a713b1a2b7de
Jaked <530974640@qq.com>
2017-08-11 17:23:41 +0800

    img background size rem

--- src/views/home/myHome/index.vue
+++ src/views/home/myHome/index.vue
@@ -5,6 +5,13 @@
                 <label class="item-label arrow" :id="'item-'+item.id">{{item.name}}</label>
             </div>
         </div>
+
+        <div class="img-rem-wrapper">
+            <h4>img作为div背景，须设置高度，用到rem，实现图片自适应</h4>
+            <div class="img-rem m-t-10"></div>
+        </div>
+
+
     </div>
 </template>
 <style lang="scss" src="./style.scss"></style>
